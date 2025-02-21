734f8ea726d74f6a8f11f265e41ef838da028325
Afriduzzaman <afriduzzaman.hoque@vega-hr.com>
2025-02-21 18:06:23 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="dental_div">
+
+      </div>
+    </>
+  );
+}
+
+export default App;
